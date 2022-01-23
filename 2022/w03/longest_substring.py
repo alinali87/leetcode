a865bed10676f2505fc73f95db871056457a8e93
alinali87 <alinali87@gmail.com>
2022-01-23 16:20:40 +0300

    add w03 two problems

---
+++ 2022/w03/longest_substring.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestSubstring(self, s: str, k: int) -> int:
+        pass
+
+
+
+
+
+def test():
+    s = Solution()
+    assert s.longestSubstring("aaabb", 3) == 3
+    assert s.longestSubstring("ababbc", 2) == 4
