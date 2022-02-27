52bbcc3352c3adb119f8251e6ff4f87a22072533
alinali87 <alinali87@gmail.com>
2022-02-27 13:48:05 +0300

    prettify formatting convert_sorted_array_to_bst

--- 2022/w09/convert_sorted_array_to_bst.py
+++ 2022/w09/convert_sorted_array_to_bst.py
@@ -1,25 +1,15 @@
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
 from typing import List, Optional
 
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
 class Solution:
-    # def helper(self, L, R, nums, root: Optional[TreeNode]):
-    #     curr = root
-    #     mid = (L + R) // 2
-    #     val = nums[mid]
-    #     if val is None or L > R:
-    #         return None
-    #     if val is not None:
-    #         root.val = val
-    #         root.left = TreeNode(None)
-    #         self.helper(L, mid - 1, nums, root.left)
-    #         root.right = TreeNode(None)
-    #         self.helper(mid + 1, R, nums, root.right)
     def helper(self, L, R, nums, parent: Optional[TreeNode], left):
         if L > R:
             return
@@ -33,10 +23,6 @@ class Solution:
                 parent.right = node
         self.helper(L, mid - 1, nums, node, left=True)
         self.helper(mid + 1, R, nums, node, left=False)
-
-
-
-
 
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
         left = 0
@@ -52,5 +38,3 @@ class Solution:
 s = Solution()
 nums = [-10,-3,0,5,9]
 root = s.sortedArrayToBST(nums)
-
-
