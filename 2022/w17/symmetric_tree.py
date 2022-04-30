999f66dc75d17745f487bf883a86f3ff4b529890
alinali87 <alinali87@gmail.com>
2022-04-30 16:47:00 +0300

    add symmetric_tree

---
+++ 2022/w17/symmetric_tree.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def validate(root1, root2):
+            if not root1 and not root2:
+                return True
+            if not root1 or not root2:
+                return False
+            if root1.val != root2.val:
+                return False
+            return validate(root1.left, root2.right) and validate(root1.right, root2.left)
+
+        return validate(root.left, root.right)
